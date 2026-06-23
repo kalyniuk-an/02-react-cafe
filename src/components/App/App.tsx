9702b0298199a59561de87ab1779e03cd42c4357
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-06-23 21:58:13 +0300

    proba vote

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import css from "./App.module.css";
 
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+
+// import { Votes } from "../../types/votes";
 
 export default function App() {
+  // const [votes, setVote] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
   return (
     <div className={css.app}>
       <CafeInfo></CafeInfo>
+      <VoteOptions></VoteOptions>
    </div>
   );
 }
